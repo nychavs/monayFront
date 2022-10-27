b76217854acb407bcd5167153a9193366d3c2bbf
nychavs <chavesnaiury@gmail.com>
2022-10-27 11:01:46 -0300

    changed the user menu

--- src/components/MenuLateral.js
+++ src/components/MenuLateral.js
@@ -6,7 +6,7 @@ function MenuLateral(){
     let controlador = 0;
     const abreFecha = (opcoes) => {
         if (controlador === 0){
-            document.getElementById(opcoes).style.height='10vh';
+            document.getElementById(opcoes).style.height='7vh';
             controlador = 1;
         }
         else{
@@ -23,8 +23,9 @@ function MenuLateral(){
                     <li>Minhas informações</li>
                     <li>Meus Contatos - Pix</li>
                 </ul>
-                </div>
-            <p onClick={()=>abreFecha('opc2')}>Extrato</p>
+            </div>
+                <div className='vl'></div>
+                <p onClick={()=>abreFecha('opc2')}>Extrato</p>
             <div id='opc2'>
                 <ul>
                     <li>Meu Extrato Atual</li>
@@ -34,21 +35,21 @@ function MenuLateral(){
             <p onClick={()=>abreFecha('opc3')}>Empréstimos</p>
             <div id='opc3'>
                 <ul>
-                    <li>Fazer um novo Empréstimo</li>
+                    <li>Fazer novo Empréstimo</li>
                     <li>Pagamento Empréstimos</li>
                 </ul>
             </div>
             <p onClick={()=>abreFecha('opc4')}>Transações</p>
             <div id='opc4'>
                 <ul>
-                    <li>Fazer uma nova Transação</li>    
-                    <li>Visualizar Transações Passadas</li>    
+                    <li>Fazer nova Transação</li>    
+                    <li>Ver Transações Passadas</li>    
                 </ul>    
             </div>
             <p onClick={()=>abreFecha('opc5')}>Cartões</p>
             <div id='opc5'>
                 <ul>
-                    <li>Visualizar Meus Cartões</li>
+                    <li>Ver Meus Cartões</li>
                     <li>Novo Cartão</li>
                 </ul>
             </div>
@@ -56,7 +57,7 @@ function MenuLateral(){
             <div id='opc6'>
                 <ul>
                     <li>Pagar Fatura</li>
-                    <li>Visualizar Minhas Faturas</li>
+                    <li>Ver Minhas Faturas</li>
                 </ul>
             </div>
         </div>
